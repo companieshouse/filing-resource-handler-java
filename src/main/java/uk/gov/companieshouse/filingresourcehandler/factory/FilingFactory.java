9babe579b97cd485ea1e7f0cb4a0f297b3bc2afb
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2026-06-25 13:35:27 +0100

    CC-3408:Added checks to handle null values
    *This pr will add go like behavior where instead of null values we overrides object to be empty the same will happen in Java resolves:[CC-3408](https://companieshouse.atlassian.net/browse/CC-3408)

--- src/main/java/uk/gov/companieshouse/filingresourcehandler/factory/FilingFactory.java
+++ src/main/java/uk/gov/companieshouse/filingresourcehandler/factory/FilingFactory.java
@@ -1,24 +1,30 @@
 package uk.gov.companieshouse.filingresourcehandler.factory;
 
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.model.filinggenerator.FilingApi;
 import uk.gov.companieshouse.api.model.transaction.Filing;
 
-import java.util.Map;
-
 @Component
 public class FilingFactory {
 
+    private static final String STATUS_PROCESSING = "processing";
+
     public Filing getFiling(FilingApi filing, String companyNumber, Map<String, String> links) {
+        if (filing == null) {
+            filing = new FilingApi();
+        }
+
         Filing patchFiling = new Filing();
         patchFiling.setCompanyNumber(companyNumber);
-        patchFiling.setDescription(filing.getDescription());
-        patchFiling.setDescriptionIdentifier(filing.getDescriptionIdentifier());
+        patchFiling.setDescription(StringUtils.defaultString(filing.getDescription()));
+        patchFiling.setDescriptionIdentifier(StringUtils.defaultString(filing.getDescriptionIdentifier()));
         patchFiling.setDescriptionValues(filing.getDescriptionValues());
         patchFiling.setLinks(links);
-        patchFiling.setStatus("processing");
-        patchFiling.setType(filing.getKind());
-        patchFiling.setCost(filing.getCost());
+        patchFiling.setStatus(STATUS_PROCESSING);
+        patchFiling.setType(StringUtils.defaultString(filing.getKind()));
+        patchFiling.setCost(StringUtils.defaultString(filing.getCost()));
         return patchFiling;
     }
 }
